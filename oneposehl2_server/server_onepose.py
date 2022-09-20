8d57486772f687ba8456429f445f8293bc73bcce
siatheindochinese <commander_sia@hotmail.com>
2022-09-20 11:51:32 +0800

    Updated server, config and calib1.txt, tested on hololens client.

--- oneposehl2_server/server_onepose.py
+++ oneposehl2_server/server_onepose.py
@@ -11,8 +11,9 @@ from scipy.spatial.transform import Rotation as R
 import threading
 import queue
 import os
-import argparse
 import io
+from concurrent import futures
+from multiprocessing.managers import BaseManager
 
 # onepose imports
 import cv2
@@ -30,18 +31,34 @@ from src.yolov5_detector import YoloV5Detector
 from pytorch_lightning import seed_everything
 from inference import load_model, pack_data
 
+class QueueManager(BaseManager):
+	pass
+
+pose_queue = queue.Queue()
+def return_pose_queue():
+	global pose_queue
+	return pose_queue
+	
+def load_lut(lut_filename):
+    with open(lut_filename, mode='rb') as depth_file:
+        lut = np.frombuffer(depth_file.read(), dtype="f")
+        lut = np.reshape(lut, (-1, 3))
+    return lut
+
 class Streamer(ultra_pb2_grpc.VTServiceServicer):
 	@torch.no_grad()
-	@hydra.main(config_path='configs/', config_name='config.yaml')
-	def __init__(self, args, qm, stop_event, cfg = cfg):
-		'''
-		args has probe, anchor, calib, device, markerdict
-		'''
-		self.mapping_probe = np.loadtxt(os.path.join('markers', args.probe + '.txt'), dtype=str)
-		self.mapping_anchor = np.loadtxt(os.path.join('markers', args.anchor + '.txt'), dtype=str)
-		self.calib = np.loadtxt(os.path.join('markers', args.calib + '.txt'), dtype=str)
-		self.left_lut = np.flip(load_lut('device_params/' + args.device + '/VLC LF_lut.bin').reshape((480,640,-1)), 0)
-		self.right_lut = np.flip(load_lut('device_params/' + args.device + '/VLC RF_lut.bin').reshape((480,640,-1)), 1)
+	def __init__(self, qm, stop_event, cfg):
+		self.mapping_probe = np.loadtxt(os.path.join('markers', cfg.probe + '.txt'), dtype=str)
+		self.mapping_anchor = np.loadtxt(os.path.join('markers', cfg.anchor + '.txt'), dtype=str)
+		self.calib = np.loadtxt(os.path.join('markers', cfg.calib + '.txt'), dtype=str)
+		self.left_lut = np.flip(load_lut('device_params/' + cfg.device + '/VLC LF_lut.bin').reshape((480,640,-1)), 0)
+		self.right_lut = np.flip(load_lut('device_params/' + cfg.device + '/VLC RF_lut.bin').reshape((480,640,-1)), 1)
+		trans = self.calib[0].astype(float)
+		rot = R.from_euler('xyz', self.calib[1].astype(float), degrees=True)
+		probe_calib7 = np.zeros(7)
+		probe_calib7[:3] = trans.squeeze()
+		probe_calib7[3:] = rot.as_quat().squeeze()
+		self.probe_calib7 = ','.join(np.round(probe_calib7, 10).astype(str))
 		
 		# queue manager
 		self.managerq = qm
@@ -81,16 +98,15 @@ class Streamer(ultra_pb2_grpc.VTServiceServicer):
 	# Detects desired object                     #
 	##############################################
 	@torch.no_grad()
-	@hydra.main(config_path='configs/', config_name='config.yaml')
 	def detect_object(self, inp):
 		if self.init == False:
-			bbox, inp_crop, K_crop = yolov5_detector.detect(inp, self.K_full)
+			bbox, inp_crop, K_crop = self.yolov5_detector.detect(inp, self.K_full)
 			self.init = True
 		else:
 			if len(self.previous_inliers) < 8:
-				bbox, inp_crop, K_crop = yolov5_detector.detect(inp, self.K_full)
+				bbox, inp_crop, K_crop = self.yolov5_detector.detect(inp, self.K_full)
 			else:
-				bbox, inp_crop, K_crop = yolov5_detector.previous_pose_detect(inp, self.K_full, self.previous_frame_pose, self.bbox3d)
+				bbox, inp_crop, K_crop = self.yolov5_detector.previous_pose_detect(inp, self.K_full, self.previous_frame_pose, self.bbox3d)
 		
 		object_detected = not (bbox == np.array([0, 0, 640, 480])).all()
 		return object_detected, bbox, inp_crop, K_crop
@@ -100,12 +116,11 @@ class Streamer(ultra_pb2_grpc.VTServiceServicer):
 	# Passes input through OnePose pipeline      #
 	##############################################
 	@torch.no_grad()
-	@hydra.main(config_path='configs/', config_name='config.yaml')
 	def oneposeFwdPass(self, inp_crop, K_crop):
 		inp_crop_cuda = torch.from_numpy(inp_crop.astype(np.float32)[None][None]/255.).cuda()
 		pred_detection = self.extractor_model(inp_crop_cuda)
 		pred_detection = {k: v[0].cpu().numpy() for k, v in pred_detection.items()}
-		inp_data = pack_data(avg_descriptors3d, clt_descriptors, keypoints3d, pred_detection, np.array([640,480]))
+		inp_data = pack_data(self.avg_descriptors3d, self.clt_descriptors, self.keypoints3d, pred_detection, np.array([640,480]))
 		pred, _ = self.matching_model(inp_data)
 		matches = pred['matches0'].detach().cpu().numpy()
 		valid = matches > -1
@@ -120,26 +135,26 @@ class Streamer(ultra_pb2_grpc.VTServiceServicer):
 		return pose_pred_homo, inliers, validcorners, notvalidcorners
 
 	@torch.no_grad()
-	@hydra.main(config_path='configs/', config_name='config.yaml')
 	def OneWay(self, request, context):
 		'''
 		Process info from request and package them into ultrapb2.PoseData format
 		context is f-ing useless lmao
 		'''
 		# drop packets from RF camera
-		if request.camera_ID == "right":
+		if request.cameraID == "Right":
 			return ultra_pb2.PoseData(USData=b'\x01',
 									  VolData = b'\x01',
 									  PoseMat = "null_pose",
-									  CalibMat = np.zeros(7),
+									  CalibMat = self.probe_calib7,
 									  AnchorMat = "null_pose",
 									  RenderFrame = "False",
 									  RenderVol = "False",
 									  MiscMsg = "Misc")
 		
+		print('LF packet received')
 		# parse image
 		inp = np.frombuffer(request.ImageData, np.uint8).reshape((480, 640))
-		inp = cv2.flip(cv2.transpose(inp), int(cameraID == "Left")) # GRAY2BGR? yolo takes RGB
+		inp = cv2.flip(cv2.transpose(inp), int(request.cameraID == "Left")) # GRAY2BGR? yolo takes RGB
 		# is this even upright? 640x480 instead of 480x640, need to check
 		
 		# parse cam2world matrix
@@ -148,28 +163,39 @@ class Streamer(ultra_pb2_grpc.VTServiceServicer):
 		
 		# onepose processing
 		##### object detection
-		object_detected, bbox, inp_crop, K_crop, init = self.detect_object(inp)
+		object_detected, bbox, inp_crop, K_crop = self.detect_object(inp)
+		if object_detected:
+			print('object detected')
+		else:
+			print('object not detected')
 		
 		##### onepose pipeline
 		if object_detected:
-			pose_pred_homo, inliers, validcorners, notvalidcorners = self.onePoseForwardPass(inp_crop, K_crop)
+			pose_pred_homo, inliers, validcorners, notvalidcorners = self.oneposeFwdPass(inp_crop, K_crop)
 			self.previous_frame_pose = pose_pred_homo
 			self.previous_inliers = inliers
+			print('OnePosed')
 		else:
 			self.previous_frame_pose = np.eye(4)
 			self.previous_inliers = []
 		
 		##### export results
 		if object_detected and not np.array_equal(pose_pred_homo, np.eye(4)):
-			result = pose_pred_homo
-			result @ LF2World
+			pose_pred_homo[1, 3] *= -1
+			pose_pred_homo = pose_pred_homo @ LF2World
+			R3 = R.from_matrix(pose_pred_homo[:-1,:-1])
+			T3 = pose_pred_homo[:-1,-1]
+			result = np.zeros(7)
+			result[:3] = T3.squeeze()                   
+			result[3:] = R3.as_quat().squeeze()
+			result = ','.join(np.round(result, 10).astype(str)) 
 		else:
 			result = "null_pose"
 		
 		USData = b'\x01'
 		VolData = b'\x01'
 		PoseMatData = result
-		CalibMatData = np.zeros(7)
+		CalibMatData = self.probe_calib7
 		AnchorMatData = "null_pose"
 		RenderFrame = "False"
 		RenderVol = "False"
@@ -185,20 +211,16 @@ class Streamer(ultra_pb2_grpc.VTServiceServicer):
 
 @torch.no_grad()
 @hydra.main(config_path='configs/', config_name='config.yaml')
-def serve(qm):
+def main(cfg):
+	os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
+	QueueManager.register('get_pose_queue', callable=return_pose_queue)
+	qm = QueueManager(address=('127.0.0.1', 5005), authkey=b'abc')
+	qm.start()
 	server = grpc.server(futures.ThreadPoolExecutor(max_workers=20))
 	
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-p', '--probe', default='m0', help="filename for marker layout")
-	parser.add_argument('-a', '--anchor', default='m1', help="filename for marker layout")
-	parser.add_argument('-c', '--calib', default='calib1', help="filename for marker-tip calibration")
-	parser.add_argument('-d', '--device', default='NUHS-HOLO1', help="specify device ID")
-	parser.add_argument('-m', '--markerdict', default='DICT_4X4_50', help="aruco dictionary")
-	args = parser.parse_args()
-	
 	stop_event = threading.Event()
-	ultra_pb2_grpc.add_VTServiceServicer_to_server(Streamer(args, qm, stop_event), server)
-	server.add_insecure_port('[::]:50061')
+	ultra_pb2_grpc.add_VTServiceServicer_to_server(Streamer(qm, stop_event, cfg), server)
+	server.add_insecure_port('[::]:50052')
 	server.start()
 	print('server start')
 	#server.wait_for_termination()
@@ -206,8 +228,4 @@ def serve(qm):
 	server.stop(5)
 
 if __name__ == '__main__':
-	os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
-	QueueManager.register('get_pose_queue', callable=return_pose_queue)
-	qm = QueueManager(address=('127.0.0.1', 5005), authkey=b'abc')
-	qm.start()
-	serve(qm)
+	main()
